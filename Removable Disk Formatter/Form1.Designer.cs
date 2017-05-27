6058bbb2f29e42140ec36261d02f8f50bc6af264
Hrishi1999 <hrishipatel99@gmail.com>
2017-05-27 15:30:57 +0530

    First Commit.

---
+++ Removable Disk Formatter/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Removable_Disk_Formatter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.size = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dname = new System.Windows.Forms.Label();
+            this.fs = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Disk : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(68, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Size : ";
+            // 
+            // size
+            // 
+            this.size.AutoSize = true;
+            this.size.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.size.Location = new System.Drawing.Point(130, 54);
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(18, 18);
+            this.size.TabIndex = 3;
+            this.size.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(68, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Label : ";
+            // 
+            // dname
+            // 
+            this.dname.AutoSize = true;
+            this.dname.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.dname.Location = new System.Drawing.Point(130, 82);
+            this.dname.Name = "dname";
+            this.dname.Size = new System.Drawing.Size(18, 18);
+            this.dname.TabIndex = 5;
+            this.dname.Text = "--";
+            // 
+            // fs
+            // 
+            this.fs.AutoSize = true;
+            this.fs.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.fs.Location = new System.Drawing.Point(130, 111);
+            this.fs.Name = "fs";
+            this.fs.Size = new System.Drawing.Size(18, 18);
+            this.fs.TabIndex = 7;
+            this.fs.Text = "--";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label5.Location = new System.Drawing.Point(68, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "FS : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(12, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 22);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Format : ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Options";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label6.Location = new System.Drawing.Point(68, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "FS : ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "NTFS",
+            "FAT32"});
+            this.comboBox2.Location = new System.Drawing.Point(132, 188);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 229);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(346, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(287, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Format";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 297);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fs);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Removable Disk Formatter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label size;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label dname;
+        private System.Windows.Forms.Label fs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Removable Disk Formatter/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Removable_Disk_Formatter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = DriveInfo.GetDrives()
+                                   .Where(x => x.DriveType == DriveType.Removable)
+                                   .ToList();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string drive_letter = comboBox1.Text.Substring(0, 1);
+            DriveInfo di = new DriveInfo(drive_letter);
+          
+            if (di.IsReady)
+            {
+                fs.Text = di.DriveFormat;
+                long toGB = di.TotalSize/ 1073741824;
+                size.Text = toGB + "GB (Approx)";
+                dname.Text = di.VolumeLabel;
+            }
+            else
+            {
+                fs.Text = "--";
+                size.Text = "--";
+                dname.Text = "--";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = DriveInfo.GetDrives()
+                                  .Where(x => x.DriveType == DriveType.Removable)
+                                  .ToList();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process dp = new Process();
+            dp.StartInfo.UseShellExecute = false;
+            dp.StartInfo.RedirectStandardOutput = true;
+            dp.StartInfo.FileName = @"C:\Windows\System32\diskpart.exe";
+            dp.StartInfo.RedirectStandardInput = true;
+            dp.Start();
+
+            dp.StandardInput.WriteLine("select disk 1"); //need immediate attention fixing this! Never use "0" though.
+            progressBar1.Value = 20;
+            dp.StandardInput.WriteLine("clean");
+            progressBar1.Value = 40;
+            dp.StandardInput.WriteLine("create partition primary");
+            progressBar1.Value = 60;
+            dp.StandardInput.WriteLine("select partition 1");
+            dp.StandardInput.WriteLine("format fs=" + comboBox2.SelectedItem.ToString() + " quick");
+            progressBar1.Value = 95;
+            dp.StandardInput.WriteLine("exit");
+            progressBar1.Value = 100;
+            string output = dp.StandardOutput.ReadToEnd();
+            dp.WaitForExit();
+
+        }
+    }
+}
